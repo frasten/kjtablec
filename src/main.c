d22ad6ba1b212712034d08232e57a9eb0dd9ef05
Marco Romano <marcojulioromano@gmail.com>
2012-05-17 15:03:12 +0200

    Removed infinite loop in main around yyparse() because it's not needed.

--- src/main.c
+++ src/main.c
@@ -4,10 +4,8 @@ int main(int argc, char *argv[]) {
 
 	printf("Hello! Welcome to kJ's Table Compiler kjtablec!\n");
 
-	while(1) {
-		yyparse();
-	}
-
+	yyparse();
+	
 	printf("Bye and thanks for flying with us!\n");
 
 	return(0);
